1d152b646045e7dd384cc9d1103b081f4def4d69
xiongkuang <xiongkuang@gmail.com>
2017-11-30 10:31:22 +0800

    [update]准备加入dubbo

---
+++ rua-manager-web/src/main/java/com/xiongkuang/controller/ItemController.java
@@ -0,0 +1,26 @@
+package com.xiongkuang.controller;
+
+import com.xiongkuang.pojo.TbItem;
+import com.xiongkuang.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by xiongkuang on 29/11/2017.
+ */
+@Controller
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/item/{itemId}")
+    @ResponseBody
+    private TbItem getItemById(@PathVariable Long itemId) {
+        TbItem tbItem = itemService.getItemById(itemId);
+        return tbItem;
+    }
+}
