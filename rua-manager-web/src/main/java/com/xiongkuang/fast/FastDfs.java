4adbbe9eaaf5dca6213b62208c3d8ac11bca0039
xiongkuang <xiongkuang@gmail.com>
2017-12-05 19:38:43 +0800

    [UPDATE]添加fastdfs支持

---
+++ rua-manager-web/src/main/java/com/xiongkuang/fast/FastDfs.java
@@ -0,0 +1,7 @@
+package com.xiongkuang.fast;
+
+/**
+ * Created by xiongkuang on 05/12/2017.
+ */
+public class FastDfs {
+}
---
+++ rua-manager-web/src/test/java/com/xiongkuang/fast/FastDfsTest.java
@@ -0,0 +1,39 @@
+package com.xiongkuang.fast;
+
+import org.csource.fastdfs.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xiongkuang on 05/12/2017.
+ */
+public class FastDfsTest {
+
+    @Test
+    public void testUpload() throws Exception{
+        //创建一个配置文件，内容为tracker服务器地址
+        //使用全剧对象来加载配置文件
+        ClientGlobal.init("/Users/xiongkuang/Desktop/practice/RuaMall/rua-manager-web/src/main/resources/conf/client.conf");
+
+        //创建TrackerClient
+        TrackerClient trackerClient = new TrackerClient();
+
+        //通过TrackerClient获得TrackerServer对象
+        TrackerServer trackerServer = trackerClient.getConnection();
+
+        //创建一个storageServer的引用，可以是null
+        StorageServer storageServer = null;
+
+        //创建一个storageClient
+        StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+
+        //使用StorageClients上传文件
+        String[] strs = storageClient.upload_file("/Users/xiongkuang/Downloads/dd.jpg", "jpg", null);
+
+        for(String s : strs){
+            System.out.println(s);
+        }
+    }
+
+}
